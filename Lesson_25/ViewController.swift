2fa55135d9ac698d1700dcf15637320263925b16
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-10-13 20:34:17 +0300

    added part 1 Lesson 25

--- Lesson_25/ViewController.swift
+++ Lesson_25/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import ContactsUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CNContactViewControllerDelegate {
+    
+    @IBOutlet weak var textLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        локализация приложения (с аргументами)(Localizable.strings)
+        textLabel.text = String(format:
+            NSLocalizedString("ViewController.HelloThisIsTestApp%@", comment: ""
+                             ), locale: Locale.current, arguments: ["Zhenya"])
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let contactsVC = CNContactPickerViewController()
+//        contactsVC.delegate = self
+//        navigationController?.present(contactsVC, animated: true, completion: nil)
     }
 
-
 }
+
+//extension ViewController: CNContactPickerDelegate {
+//
+//    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+//        let contactDetailsVC = CNContactViewController(for: contact)
+//        contactDetailsVC.allowsEditing = true
+//        contactDetailsVC.allowsActions = true
+//        contactDetailsVC.delegate = self
+//        navigationController?.pushViewController(contactDetailsVC, animated: true)
+//
+//    }
+//
+//    func contactPicker(_ picker: CNContactPickerViewController, didSelect contactProperty: CNContactProperty) {
+//
+//    }
+//
+//}
 

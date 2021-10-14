0f1b5e84d6bd74fd59d16a371717120150465056
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-10-14 17:46:13 +0300

    added part 2 Lesson-25

--- Lesson_25/ViewController.swift
+++ Lesson_25/ViewController.swift
@@ -11,24 +11,45 @@ import ContactsUI
 class ViewController: UIViewController, CNContactViewControllerDelegate {
     
     @IBOutlet weak var textLabel: UILabel!
-
+    let contactStore = CNContactStore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        локализация приложения (с аргументами)(Localizable.strings)
+        //        локализация приложения (с аргументами)(Localizable.strings)
         textLabel.text = String(format:
-            NSLocalizedString("ViewController.HelloThisIsTestApp%@", comment: ""
-                             ), locale: Locale.current, arguments: ["Zhenya"])
+                                    NSLocalizedString("ViewController.HelloThisIsTestApp%@", comment: ""
+                                                     ), locale: Locale.current, arguments: ["Zhenya"])
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-//        let contactsVC = CNContactPickerViewController()
-//        contactsVC.delegate = self
-//        navigationController?.present(contactsVC, animated: true, completion: nil)
+        //        let contactsVC = CNContactPickerViewController()
+        //        contactsVC.delegate = self
+        //        navigationController?.present(contactsVC, animated: true, completion: nil)
+        
+        let keys: [CNKeyDescriptor] = [CNContactPhoneNumbersKey as CNKeyDescriptor, CNContactGivenNameKey as CNKeyDescriptor, CNContactNameSuffixKey as CNKeyDescriptor]
+        let predicate = NSPredicate(format: "phoneNumbers.@count > 0")
+        let contacts = try? contactStore.unifiedContacts(matching: predicate, keysToFetch: keys)
+        
+        
+        
+//        создание контактов
+        let saveRequest = CNSaveRequest()
+        let newContact = CNMutableContact()
+        newContact.phoneNumbers.append(CNLabeledValue(
+            label: CNLabelPhoneNumberMain,
+            value: CNPhoneNumber(stringValue: "+37529566454543"
+                                )
+        )
+        )
+        newContact.givenName = "Zhenya"
+        saveRequest.add(newContact, toContainerWithIdentifier: nil)
+        
+        try? contactStore.execute(saveRequest)
     }
-
+    
 }
 
 //extension ViewController: CNContactPickerDelegate {
